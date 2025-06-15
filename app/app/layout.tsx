02c7a634ea35c27f130fa36ae98b203f5914b0d7
EmilAnkerstjerne <emil.ankerstjerne@gmail.com>
2025-06-15 09:27:32 +0200

    fix

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -9,9 +9,9 @@ import { APIFetch } from "@/lib/utils"
 import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
 
-export const iframeHeight = "800px"
+// export const iframeHeight = "800px"
 
-export const description = "A sidebar with a header and a search form."
+// export const description = "A sidebar with a header and a search form."
 
 export default function Layout({
   children
